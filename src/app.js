4c22a32429f85ddc1e8f313a55ff7554ddfe3558
kikegal <kikedosul@gmail.com>
2023-05-26 13:21:41 -0300

    pre-render

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import toursRoutes from './routes/tours.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
 import {PORT} from './config.js'
+import morgan from 'morgan'
 
 const app = express() 
 
 app.use (express.json())
+
+app.use(morgan('dev'))
 
 app.use(indexRoutes)
 app.use('/api', toursRoutes)
